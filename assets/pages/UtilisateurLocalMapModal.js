064d31f81ed66b02948deb66c2f2745f66255bc2
Mathurin Petit <mathurin.petit@gmail.com>
2022-03-29 03:17:35 +0200

    lien et carte popup

--- assets/pages/UtilisateurLocalMapModal.js
+++ assets/pages/UtilisateurLocalMapModal.js
@@ -3,6 +3,9 @@ import Layout from "../components/Layout";
 import { MapContainer, Marker, Popup, TileLayer, FeatureGroup } from 'react-leaflet';
 import axios from 'axios';
 import {Modal, Button} from 'react-bootstrap';
+import L from 'leaflet';
+import Double from "../images/icons/Double.png";
+import Coupe from "../images/icons/Coupe.png";
 
 
 const centerMap = [48.902170, 2.400660] //Oscillo studio
@@ -14,6 +17,23 @@ function UtilisateurLocalMapModal(props) {
   const [map, setMap] = useState(null);
   const [instrumentsMarkerList, setInstrumentsMarkerList] = useState([])
   const featureGroupRef = useRef();
+
+  let iconsArray = {
+    "DoubleIcon": L.icon({
+      iconUrl: Double,
+      iconRetinaUrl: Double,
+      iconAnchor: [35, 30],
+      popupAnchor: [10, -44],
+      iconSize: [60, 60],
+    }),
+    "CoupeIcon": L.icon({
+      iconUrl: Coupe,
+      iconRetinaUrl: Coupe,
+      iconAnchor: [35, 30],
+      popupAnchor: [10, -44],
+      iconSize: [60, 60],
+    })
+  }
 
   const handleClose = () => {
     setShow(false);
@@ -40,6 +60,7 @@ function UtilisateurLocalMapModal(props) {
         <Modal show={show} onHide={handleClose} size="lg" >
           <Modal.Header closeButton>
             <Modal.Title>
+              <h3>{props.instrument.name} - {props.instrument.type} - Chez <strong>{props.instrument.emprunteurNom}</strong></h3>
               <h5>
                 {props.instrument.emprunteurAdresse}
               </h5>
@@ -57,9 +78,16 @@ function UtilisateurLocalMapModal(props) {
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
             <FeatureGroup ref={featureGroupRef}>
-              <Marker position={[props.instrument.emprunteurLat,props.instrument.emprunteurLon]} className={"markerInstru "+props.instrument.type} >
-                 <Popup>
-                   {props.instrument.type} <br /> {props.instrument.name}
+              <Marker position={[props.instrument.emprunteurLat,props.instrument.emprunteurLon]} className={"markerInstru "+props.instrument.type}
+                icon={ iconsArray[instrument.type+"Icon"] } >
+                    <Popup className="mapMiniPopup" maxWidth="350" >
+                     <h3>{props.instrument.name} - {props.instrument.type}</h3>
+                     <small>{props.instrument.description}</small><br />
+                     <h4><strong>{props.instrument.emprunteurNom}</strong></h4>
+                     <h6><strong>{props.instrument.emprunteurAdresse}</strong></h6>
+                     <button className="btn btn-sm btn-light" onClick={ (e) => { window.location.href="geo:"+props.instrument.emprunteurLat+","+props.instrument.emprunteurLon } }  >
+                         J'y vais&nbsp;&nbsp;&nbsp;&nbsp;<ion-icon name="navigate-outline"></ion-icon>
+                     </button>
                  </Popup>
                </Marker>
             </FeatureGroup>
@@ -70,8 +98,8 @@ function UtilisateurLocalMapModal(props) {
             <Button variant="secondary" onClick={handleClose}>
               Fermer
             </Button>
-            <Button className="btn btn-primary">
-              J'Y VAIS
+            <Button className="btn btn-primary" onClick={ (e) => { window.location.href="geo:"+props.instrument.emprunteurLat+","+props.instrument.emprunteurLon+"?q="+props.instrument.emprunteurLat+","+props.instrument.emprunteurLon } } >
+                J'y vais&nbsp;&nbsp;&nbsp;&nbsp;<ion-icon name="navigate-outline"></ion-icon>
             </Button>
           </Modal.Footer>
         </Modal>
